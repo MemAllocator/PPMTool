4a4173c7985273043f328aa9c20023c8d90d4ee1
MemAllocator <omerkisoss@gmail.com>
2018-10-22 22:15:01 +0300

    first commit

---
+++ PPMToolProject/src/main/java/com/rest/PPMToolProject/PpmToolProjectApplication.java
@@ -0,0 +1,12 @@
+package com.rest.PPMToolProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolProjectApplication.class, args);
+	}
+}
